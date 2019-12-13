c8b9138cfdb217ea5f4609b40ef54b9dfad6ec9c
ZNOW0 <47673358+ZNOW0@users.noreply.github.com>
2019-12-13 12:39:33 +0100

    Update Passcode_sys.py

--- Passcode_sys.py
+++ Passcode_sys.py
@@ -3,50 +3,56 @@ import time
 from random import randint
 from time import perf_counter
 
-passcode_input = "X"
+
 passcode = "X"
-State = True
+passcode_input = "X"
+PassState = True
+CheckState = True
 
 def passcode_set():
     global passcode
-    global State
+    global PassState
 
-    State = True
 
-    while State:
+    while PassState:
         try:
-            passcode = int(input("Enter your passcode of choice:"))
+            passcode = int(input("Enter passcode of choice:"))
+            PassState = False
             print("passcode sucsesfully set")
-            State = False
         except:
             print("Only numbers are allowed")
 
 def passcode_check():
+    global CheckState
     global passcode_input
-    global State
 
-    State = True
+    CheckState = True
     Passcode_count = 0
 
 
-    while State:
+    while CheckState:
         try:
-            passcode_input = int(input("Enter auth:"))
+            passcode_input = int(input("Enter passcode for auth:"))
         except:
             pass
 
         if passcode_input == passcode:
             print("Correct passcode, acces granted")
-            State = False
+            CheckState = False
         else:
             print("passcode incorrect")
             Passcode_count += 1
             if Passcode_count == 3:
+                CheckState = False
                 print("Too many tries")
-                State = False
             else:
                 pass
         
+
+def pass_save():
+    Pass = open("password.txt", "w+")
+    Pass.write(str(passcode_input))
+    Pass.close()
+
 passcode_set()
 passcode_check()
-
